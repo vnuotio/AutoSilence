b9566fcdee15dd446fadd630b5d72c106339fb0d
Vertti Nuotio <vertti.nuotio@gmail.com>
2023-03-23 19:51:53 +0200

    framework for requesting and changing permission

--- app/src/main/java/fi/vnuotio/autosilence/MainActivity.kt
+++ app/src/main/java/fi/vnuotio/autosilence/MainActivity.kt
@@ -1,11 +1,29 @@
 package fi.vnuotio.autosilence
 
+import android.app.NotificationManager
+import android.content.Intent
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nm: NotificationManager
+    private lateinit var am: AudioManager
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nm = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        am = getSystemService(AUDIO_SERVICE) as AudioManager
+
+        if (!nm.isNotificationPolicyAccessGranted) requestNotificationPermissions()
+    }
+
+    private fun requestNotificationPermissions() {
+        val intent = Intent(ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+        startActivity(intent)
     }
 }
