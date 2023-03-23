73c4b2ec347b849711d284fff8e53ab34b80aac5
Vertti Nuotio <vertti.nuotio@gmail.com>
2023-03-23 21:18:31 +0200

    add/move permission logic to the switch and prevent crashes if permission for 'Do not disturb' isn't given

--- app/src/main/java/fi/vnuotio/autosilence/MainActivity.kt
+++ app/src/main/java/fi/vnuotio/autosilence/MainActivity.kt
@@ -1,25 +1,61 @@
 package fi.vnuotio.autosilence
 
 import android.app.NotificationManager
+import android.content.DialogInterface
 import android.content.Intent
 import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
 import androidx.appcompat.widget.SwitchCompat
+import fi.vnuotio.autosilence.utils.SharedPrefsHandler
+import fi.vnuotio.autosilence.utils.createNeutralPopup
 
 class MainActivity : AppCompatActivity() {
     private lateinit var nm: NotificationManager
     private lateinit var am: AudioManager
-    
+    private lateinit var sph: SharedPrefsHandler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         nm = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         am = getSystemService(AUDIO_SERVICE) as AudioManager
+        sph = SharedPrefsHandler(applicationContext)
 
-        if (!nm.isNotificationPolicyAccessGranted) requestNotificationPermissions()
+        setSwitchActions()
+    }
+
+    private fun setSwitchActions() {
+        val switch = findViewById<SwitchCompat>(R.id.statusSwitch)
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            val isPermissionGranted = nm.isNotificationPolicyAccessGranted
+
+            if (isChecked) {
+                if (!isPermissionGranted) {
+                    switch.isChecked = false
+                    showPermissionPopup()
+                    return@setOnCheckedChangeListener
+                }
+                sph.setLastRingerMode(am.ringerMode)
+                // TODO: Enable background functionality
+            }
+            else {
+                if (isPermissionGranted) am.ringerMode = sph.getLastRingerMode()
+                // TODO: Disable background functionality
+            }
+        }
+    }
+
+    private fun showPermissionPopup() {
+        val listener = DialogInterface.OnClickListener {_, _ ->
+            requestNotificationPermissions()
+        }
+        createNeutralPopup(this,
+            "Permission required",
+            "'Do not disturb' access is required for this app to function",
+            listener)
     }
 
     private fun requestNotificationPermissions() {
