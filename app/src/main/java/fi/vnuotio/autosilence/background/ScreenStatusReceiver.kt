68a319b4691c638af9e6fc693c593350a5ed5efd
Vertti Nuotio <vertti.nuotio@gmail.com>
2023-03-25 01:04:56 +0200

    implement setting ringer modes on screen on/off change

--- app/src/main/java/fi/vnuotio/autosilence/background/ScreenStatusReceiver.kt
+++ app/src/main/java/fi/vnuotio/autosilence/background/ScreenStatusReceiver.kt
@@ -1,8 +1,18 @@
 package fi.vnuotio.autosilence.background
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.AUDIO_SERVICE
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.content.Intent.ACTION_SCREEN_OFF
+import android.content.Intent.ACTION_SCREEN_ON
+import android.media.AudioManager
+import android.media.AudioManager.RINGER_MODE_SILENT
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+import fi.vnuotio.autosilence.utils.SharedPrefsHandler
 
 class ScreenStatusReceiver : BroadcastReceiver() {
     companion object {
@@ -12,7 +22,21 @@ class ScreenStatusReceiver : BroadcastReceiver() {
             synchronized(this) { return INSTANCE ?: ScreenStatusReceiver() }
         }
     }
-    override fun onReceive(p0: Context?, p1: Intent?) {
-        TODO("Not yet implemented")
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null || intent == null) return
+
+        val nm = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val am = context.getSystemService(AUDIO_SERVICE) as AudioManager
+        val sph = SharedPrefsHandler(context)
+
+        if (!nm.isNotificationPolicyAccessGranted) return
+
+        if (intent.action == ACTION_SCREEN_ON) {
+            am.ringerMode = RINGER_MODE_SILENT
+        }
+        else if (intent.action == ACTION_SCREEN_OFF) {
+            am.ringerMode = sph.getLastRingerMode()
+        }
     }
 }
