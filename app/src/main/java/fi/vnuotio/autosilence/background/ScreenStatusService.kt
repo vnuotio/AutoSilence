9501d1c84787b4f6b1ed9d83ea4151de53fb2a6d
Vertti Nuotio <vertti.nuotio@gmail.com>
2023-03-25 00:46:08 +0200

    implement background service start and stop

--- app/src/main/java/fi/vnuotio/autosilence/background/ScreenStatusService.kt
+++ app/src/main/java/fi/vnuotio/autosilence/background/ScreenStatusService.kt
@@ -2,16 +2,38 @@ package fi.vnuotio.autosilence.background
 
 import android.app.Service
 import android.content.Intent
+import android.content.Intent.ACTION_SCREEN_OFF
+import android.content.Intent.ACTION_SCREEN_ON
+import android.content.IntentFilter
 import android.os.IBinder
 
 class ScreenStatusService : Service() {
     companion object {
         private var INSTANCE: ScreenStatusService? = null
+        private lateinit var receiver: ScreenStatusReceiver
 
         fun getService(): ScreenStatusService? {
             return INSTANCE
         }
     }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        receiver = ScreenStatusReceiver.getInstance()
+        val filter = IntentFilter()
+
+        filter.addAction(ACTION_SCREEN_ON)
+        filter.addAction(ACTION_SCREEN_OFF)
+
+        registerReceiver(receiver, filter)
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
+
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
